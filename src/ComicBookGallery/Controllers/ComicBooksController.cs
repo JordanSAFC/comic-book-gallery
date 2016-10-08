ef8416631529edcf3d42fc1f0647f678e084568a
JordanHC <jordanhc1@hotmail.co.uk>
2016-10-09 00:40:25 +0100

    Add if statement regarding the day of the week

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,13 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller");
+        }
     }
 }
